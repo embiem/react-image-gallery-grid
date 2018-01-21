6a6dfabdbfe4bfb2b7c70107bab7316a999d61a4
Martin Beierling <mbeierling@gmail.com>
2018-01-21 17:36:33 +0100

    Initial commit with first version

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,25 @@
-import React, {Component} from 'react'
-import {render} from 'react-dom'
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-import Example from '../../src'
+import "./index.css";
+import ImageGallery from "../../src";
 
 class Demo extends Component {
   render() {
-    return <div>
-      <h1>react-image-gallery-grid Demo</h1>
-      <Example/>
-    </div>
+    return (
+      <div>
+        <h1>react-image-gallery-grid Demo</h1>
+        <h2>default</h2>
+        <ImageGallery />
+
+        <h2>800px fixed-width</h2>
+        <ImageGallery style={{width: "800px"}} />
+
+        <h2>custom max-spannings of >3 ^1</h2>
+        <ImageGallery maxHorizontal={3} maxVertical={1} />
+      </div>
+    );
   }
 }
 
-render(<Demo/>, document.querySelector('#demo'))
+render(<Demo />, document.querySelector("#demo"));
--- src/index.js
+++ src/index.js
@@ -1,9 +1,122 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
+
+import "./index.css";
+
+function randomNumber(limit) {
+  return Math.floor(Math.random() * limit) + 1;
+}
+
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
+
+function createRandomSpannings(maxHorizontal, maxVertical) {
+  // define defaults
+  maxHorizontal = typeof maxHorizontal === "undefined" ? 4 : maxHorizontal;
+  maxVertical = typeof maxVertical === "undefined" ? 4 : maxVertical;
+
+  const arr = [
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    [1, 1],
+    ...Array.from(
+      {
+        length: 50
+      },
+      () => [randomNumber(maxHorizontal), randomNumber(maxVertical)]
+    )
+  ];
+  shuffle(arr);
+  return arr;
+}
 
 export default class extends Component {
+  static defaultProps = {
+    images: [
+      "https://source.unsplash.com/random/300x300?v=1",
+      "https://source.unsplash.com/random/300x300?v=2",
+      "https://source.unsplash.com/random/300x300?v=3",
+      "https://source.unsplash.com/random/300x300?v=4",
+      "https://source.unsplash.com/random/300x300?v=5",
+      "https://source.unsplash.com/random/300x300?v=6",
+      "https://source.unsplash.com/random/300x300?v=7",
+      "https://source.unsplash.com/random/300x300?v=8",
+      "https://source.unsplash.com/random/300x300?v=9"
+    ],
+    maxHorizontal: 4,
+    maxVertical: 4
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      overlayImage: "",
+      randomSpannings: createRandomSpannings(
+        props.maxHorizontal,
+        props.maxVertical
+      )
+    };
+  }
+
   render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
+    return (
+      <div>
+        <div className={`overlay ${this.state.overlayImage ? "open" : ""}`}>
+          <div className="overlay-inner">
+            <button
+              className="close"
+              onClick={() => this.setState({ overlayImage: "" })}
+            >
+              × Close
+            </button>
+            <img src={this.state.overlayImage} />
+          </div>
+        </div>
+
+        <section className="gallery" style={this.props.style}>
+          {this.props.images.map((img, idx) => {
+            const currentSpannings = this.state.randomSpannings[
+              idx % this.state.randomSpannings.length
+            ];
+            const h = currentSpannings[0];
+            const v = currentSpannings[1];
+            return (
+              <div
+                key={idx}
+                className={`item h${h} v${v}`}
+                onClick={e => {
+                  this.setState({ overlayImage: img });
+                }}
+              >
+                <img src={img} />
+                <div className="item__overlay">
+                  <button>View →</button>
+                </div>
+              </div>
+            );
+          })}
+        </section>
+      </div>
+    );
   }
 }
